42d86dfce380ab900bdbb7dad0f87f8026cc7a27
anvarsosnitski <anvar.sosnitski@gmail.com>
2014-12-04 11:28:00 +0000

    First commit

---
+++ project/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name projectApp
+ * @description
+ * # projectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('projectApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
