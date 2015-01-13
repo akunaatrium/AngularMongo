2f5e71b65cb0b385978b4e997e74b3eea6c10488
Anvar Sosnitski <anvar.sosnitski@gmail.com>
2015-01-13 23:03:32 +0200

    Added two tests to backend.

--- backend/test/test.js
+++ backend/test/test.js
@@ -3,6 +3,7 @@ process.env.NODE_ENV = "test";
 var should = require('should');
 var supertest = require('supertest');
 var async = require('async');
+var _ = require('lodash');
 
 var mongoose = require('mongoose');
 var Comic = require('../models/comic');
@@ -18,7 +19,7 @@ var dropDatabase = function (callback) {
 };
 
 describe('Comics API', function () {
-	var comics = [
+	var COMICS = [
 		{
 			type: 'Garfield',
 			urlPattern: 'https://garfield.com/uploads/strips/[YYYY-MM-DD].jpg'
@@ -26,25 +27,34 @@ describe('Comics API', function () {
 		{
 			type: 'Scarygoround',
 			urlPattern: 'http://scarygoround.com/strips/[YYYYMMDD].png'
+		},
+		{
+			type: 'hello',
+			urlPattern: 'world'
 		}
 	];
 
 	beforeEach(function (done) {
-		console.log('Adding two comics to test database.');
-		var comic1 = new Comic(comics[0]);
-		var comic2 = new Comic(comics[1]);
+		console.log('Adding three comics to test database.');
+		var comic1 = new Comic(COMICS[0]);
+		var comic2 = new Comic(COMICS[1]);
+		var comic3 = new Comic(COMICS[2]);
 
 		async.parallel([
 			function (callback) { comic1.save(callback); },
 			function (callback) { comic2.save(callback); },
+			function (callback) { comic3.save(callback); },
 			],
 			done
 		);
 	});
 
-	describe('GET /comics', function () {
+	afterEach(function (done) {
+		console.log('afterEach function called (which should drop database)');
+		dropDatabase(done);
+	});
 
-		afterEach(function (done) { dropDatabase(done); });
+	describe('GET /comics', function () {
 
 		// Tests.
 		it('should return successful result', function (done) {
@@ -63,11 +73,11 @@ describe('Comics API', function () {
 				.end(done);
 		});
 
-		it('should return array of length 2', function (done) {
+		it('should return array of length 3', function (done) {
 			supertest(app).get('/comics')
 				.end(function (err, res) {
 					var comics = res.body;
-					comics.should.be.an.Array.and.have.lengthOf(2);
+					comics.should.be.an.Array.and.have.lengthOf(3);
 					done();
 				});
 		});
@@ -76,43 +86,101 @@ describe('Comics API', function () {
 			supertest(app).get('/comics')
 				.end(function (err, res) {
 					var comics = res.body;
-					comics.should.containEql(comics[0]);
-					comics.should.containEql(comics[1]);
+					_.forEach(comics, function(comic, index, collection) {
+						var strippedComic = {type: comic.type, urlPattern: comic.urlPattern};
+						COMICS.should.containEql(strippedComic);
+					});
 					done();
 				});
 		});
-
 	}); // GET /comics
 
 	describe('GET /comics/:id', function () {
 
-		var objectId;
-		var comicData;
+		var specialComic = {
+			_id: null,
+			type: 'foo',
+			urlPattern: 'bar'
+		};
 
 		before(function (done) {
-			console.log('Adding a comic with specific ID to test database.');
-			objectId = new mongoose.mongo.ObjectID();
-			comicData = {_id: objectId, type: 'hello', urlPattern: 'world'};
-			var comic = new Comic(comicData);
+			console.log('Adding comic with special ID to test database.');
+			var objectId = new mongoose.mongo.ObjectID();
+			specialComic._id = objectId;
+			var comic = new Comic(specialComic);
 			comic.save(done);
 		});
 
 		it('should return the right comic', function (done) {
-			supertest(app).get('/comics/' + objectId)
+			supertest(app).get('/comics/' + specialComic._id)
 				.end(function (err, res) {
 					var comic = res.body;
-					comic.should.have.property('type', 'hello');
-					comic.should.have.property('urlPattern', 'world');
-					comic.should.have.property('_id', objectId.toHexString());
+					comic.should.have.property('type', specialComic.type);
+					comic.should.have.property('urlPattern', specialComic.urlPattern);
+					comic.should.have.property('_id', specialComic._id.toHexString());
 					done();
 				});
+		});
+	}); // GET /comics/:id
+
+	describe('PUT /comics/:id', function () {
+
+		var specialComic = {
+			_id: null,
+			type: 'foo',
+			urlPattern: 'bar'
+		};
+
+		before(function (done) {
+			console.log('Adding comic with special ID to test database.');
+			var objectId = new mongoose.mongo.ObjectID();
+			specialComic._id = objectId;
+			var comic = new Comic(specialComic);
+			comic.save(done);
+		});
+
+		it('should be possible to request updating a comic and the response should be the comic with updated values', function (done) {
+			supertest(app).put('/comics/' + specialComic._id)
+				.send({type: 'foo', urlPattern: 'burrito'})
+				.expect(200)
+				.end(function (err, res) {
+					var comic = res.body;
+					comic.should.have.property('_id', specialComic._id.toHexString());
+					comic.should.have.property('type', 'foo');
+					comic.should.have.property('urlPattern', 'burrito');
+					done();
+				})
+		});
+
+		it.only('should actually update the comic in the database', function (done) {
+			async.series([
+				function (callback) {
+					supertest(app).put('/comics/' + specialComic._id)
+						.send({type: 'foo', urlPattern: 'burrito'})
+						.expect(200)
+						.end(callback);
+				},
+				function (callback) {
+					supertest(app).get('/comics/' + specialComic._id)
+						.expect(200)
+						.end(function (err, res) {
+							var comic = res.body;
+							comic.should.have.property('_id', specialComic._id.toHexString());
+							comic.should.have.property('type', 'foo');
+							comic.should.have.property('urlPattern', 'burrito');
+							callback();
+						});
+				}
+				],
+				done);
 		});
 	});
 
 	after(function (done) {
-		console.log('Closing mongodb connection.');
-		mongoose.connection.close(function () {
-			done();
-		});
+		async.series([
+			function (callback) { dropDatabase(callback); }, // Probably should not drop here becaus eit is dropped anyway after each test.
+			function (callback) { mongoose.connection.close(callback); }
+			],
+			done);
 	});
 });
